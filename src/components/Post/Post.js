c2ed72c69b999d2f769e6f6ea811f7b68d421738
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-21 23:20:56 +0300

    post.js ile ilk api istegini atma ve postlari frontende cekme

---
+++ src/components/Post/Post.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function Post(){
+    const[error, setError] = useState(null);
+    const[isLoaded, setIsLoaded] = useState(false);
+    const[posts, setPosts] = useState([]);
+
+    useEffect(() =>{
+        fetch("/posts")
+        .then(res => res.json())
+        .then(
+            (result)=> {
+                setIsLoaded(true);
+                setPosts(result);
+            },
+            (error) =>{
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [])
+
+    if(error){
+        return <div>Error: {error.message}</div>;
+    } else if(!isLoaded){
+        return <div>Loading ...</div>
+    } else{
+        return(
+            <ul>
+                {posts.map(post =>(
+                    <li>
+                        {post.title}{post.text}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default Post;
